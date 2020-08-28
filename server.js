820c3d68818616ec54f3e776cd8eba22d2442ab3
jeongsooyoung <code.ameba@gmail.com>
2020-08-29 00:24:08 +0900

    get api

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get('/api/customers', (req, res) => {
   res.send([
